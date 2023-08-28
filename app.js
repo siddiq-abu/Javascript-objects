c84aa8caa51d955d0588aff15fb937ca22468e19
siddiq-abu <abusiddiq200g3@gmail.com>
2023-08-28 10:13:56 +0000

    javascript-objects

---
+++ app.js
@@ -0,0 +1,10 @@
+let users = [
+    {
+        username:'siddiq',
+        email:'siddiq@gmail.com',
+        subsriptionStatus:'VIP',
+        discordIn:'Siddiq#001',
+        lessonsCompleted:[0,1]
+    }
+]
+
